2bdbe08de7c5621c7461cfc5197bef0073c7f5a5
Sudhi S. <sudhiselampa19@gmail.com>
2025-01-12 15:54:30 +0530

    #GETLOGS-Pramaan

--- controllers/reconStatusController.js
+++ controllers/reconStatusController.js
@@ -1,5 +1,8 @@
 const { v4: uuidv4 } = require("uuid");
 const axios = require("axios");
+const { createAuthorizationHeader } = require(".");
+
+let transaction_ids = [];
 
 const reconStatusController = async (req, res) => {
   try {
@@ -11,7 +14,6 @@ const reconStatusController = async (req, res) => {
       context: {
         ...context,
         timestamp: new Date().toISOString(),
-        transaction_id:'972acef3-d36f-4102-b644-3b3fb5406cd6',
         message_id: uuidv4(),
         bap_id: "ondc.eatiko.com",
         bap_uri: "https://ondc.eatiko.com/ondc-PREPROD",
@@ -23,12 +25,27 @@ const reconStatusController = async (req, res) => {
       data.context.bpp_id = "pramaan.ondc.org/beta/preprod/mock/seller";
       data.context.bpp_uri =
         "https://pramaan.ondc.org/beta/preprod/mock/seller";
+      data.context.transaction_id = uuidv4();
     }
 
-    const response = await axios.post(`${baseUrl}/${subUrl}`, data);
-    
-    // Only return the data portion of the Axios response
-    res.status(200).json({response:response.data,data:data});
+    if (context.action !== "select") {
+      data.context.transaction_id = uuidv4();
+      transaction_ids.push(data.context.transaction_id);
+    } else if (context.action !== "search") {
+      data.context.transaction_id = transaction_ids[transaction_ids.length - 1];
+    }
+    const authorizationHeader = await createAuthorizationHeader(data);
+
+    const response = await axios.post(`${baseUrl}/${subUrl}`, data,{
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: authorizationHeader,
+        "X-Gateway-Authorization": authorizationHeader,
+      },
+    });
+
+    // Return only the data portion of the Axios response
+    res.status(200).json({ response: response.data, data: data });
   } catch (error) {
     return res
       .status(500)
